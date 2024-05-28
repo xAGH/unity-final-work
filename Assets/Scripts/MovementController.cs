b1bdc223d1eff785c5a4019ac7dfe3bd48b89f86
Alejo Giraldo <alejito23001@gmail.com>
2024-05-28 02:29:34 -0500

    feat: crystals generation with map

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class MovementController : MonoBehaviour
@@ -6,11 +7,47 @@ public class MovementController : MonoBehaviour
     public Joystick joystick;
     public float moveSpeed;
     private float hInput, vInput;
+    private Animator animator;
+    private int direction;
+    private float lastAnimationUpdateTime;
+
+    private void Start() {
+        animator = GetComponent<Animator>();
+        lastAnimationUpdateTime = Time.time;
+    }
+
+    // private void FixedUpdate() {
+    //     if (joystick == null) return;
+    //     hInput = joystick.Horizontal * moveSpeed;
+    //     vInput = joystick.Vertical * moveSpeed;
+    //     if (Math.Abs(hInput) > Math.Abs(vInput))  direction = hInput > 0 ? 2 : 3;
+    //     else direction = vInput > 0 ? 1 : 0;
+    //     animator.SetInteger("Direction", direction);
+    //     transform.Translate(hInput, vInput, 0);
+    // }
+
+
 
     private void FixedUpdate() {
         if (joystick == null) return;
-        hInput = joystick.Horizontal * moveSpeed;
-        vInput = joystick.Vertical * moveSpeed;
-        transform.Translate(hInput, vInput, 0);
+
+        Vector2 input = new Vector2(joystick.Horizontal, joystick.Vertical).normalized;
+        int direction = GetMovementDirection(input);
+        Vector3 move = transform.position + moveSpeed * (Vector3)input;
+        transform.position = Vector3.Lerp(transform.position, move, 0.1f);
+
+        if (Time.time - lastAnimationUpdateTime > 0.1f) {
+            animator.SetInteger("Direction", direction);
+            lastAnimationUpdateTime = Time.time;
+        }
     }
+
+    private int GetMovementDirection(Vector2 input) {
+        if (Mathf.Abs(input.x) > Mathf.Abs(input.y)) {
+            return input.x > 0 ? 2 : 3;
+        } else {
+            return input.y > 0 ? 1 : 0;
+        }
+    }
+
 }
--- Assets/Scripts/SpawnCrystals.cs
+++ Assets/Scripts/SpawnCrystals.cs
@@ -4,26 +4,37 @@ public class SpawnCrystals : MonoBehaviour
 {
 
     private float maxY, minY, maxX, minX;
-    public  GameObject parent;
+
+    public Transform topBoundary;
+    public Transform bottomBoundary;
+    public Transform leftBoundary;
+    public Transform rightBoundary;
     public GameObject[] crystals;
 
     void Awake()
     {
-        maxY = GameObject.Find("Boundaries/Top").transform.position.y - 1; 
-        minY = GameObject.Find("Boundaries/Bottom").transform.position.y + 1;
-        maxX = GameObject.Find("Boundaries/Right").transform.position.x - 1;
-        minX = GameObject.Find("Boundaries/Left").transform.position.x + 1;
+        maxY = topBoundary.position.y - 1; 
+        minY = bottomBoundary.position.y + 1;
+        maxX = rightBoundary.position.x - 1;
+        minX = leftBoundary.position.x + 1;
 
         GenerateCrystals();
     }
 
     void GenerateCrystals() {
-        foreach (GameObject crystal in crystals) {
+        GameObject parent = new();
+        int crystalsLength = crystals.Length;
+        int i = 0;
+        while (i < crystalsLength) {
             float x = Random.Range(minX, maxX);
             float y = Random.Range(minY, maxY);
-
-            GameObject crystalInstance = Instantiate(crystal, new Vector3(x, y, 0), Quaternion.identity);
-            crystalInstance.transform.parent = parent.transform;
+            Vector3 position = new(x, y, 0);
+            Collider[] intersecting = Physics.OverlapSphere(position, 1f);
+            if (intersecting.Length == 0) {
+                GameObject crystal = Instantiate(crystals[i], position, Quaternion.identity, parent.transform);
+                crystal.GetComponent<Renderer>().sortingLayerID = SortingLayer.NameToID("Layer 3");
+                i++;
+            }
         }
     }
 
