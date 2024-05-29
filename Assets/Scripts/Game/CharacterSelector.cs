221b439af58e75531f6bd8d7deb2d9df63cb398f
Alejo Giraldo <alejito23001@gmail.com>
2024-05-29 16:44:15 -0500

    feat: characters animations

--- Assets/Scripts/Game/CharacterSelector.cs
+++ Assets/Scripts/Game/CharacterSelector.cs
@@ -1,18 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelector : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public GameObject[] characters;
+    public TMP_Text characterName;
+    private int selectedCharacter;
+
+    void SetName() {
+        characterName.text = characters[selectedCharacter].name;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        selectedCharacter = 0;
+        for(int i = 0; i < characters.Length; i++) {
+            if (i == selectedCharacter) characters[i].SetActive(true);
+            else  characters[i].SetActive(false);
+        }
+        SetName();
+    }
+
+    public void StartGame() {
+        PlayerPrefs.SetInt("selectedCharacter", selectedCharacter);
+        SceneManager.LoadScene("SceneSelector", LoadSceneMode.Single);
+    }
+
+    public void Next() {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter = (selectedCharacter + 1) % characters.Length;
+        characters[selectedCharacter].SetActive(true);
+        SetName();
+    }
+
+    public void Previous() {
+        characters[selectedCharacter].SetActive(false);
+        selectedCharacter--;
+        if (selectedCharacter < 0) {
+            selectedCharacter += characters.Length;
+        }
+        characters[selectedCharacter].SetActive(true);
+        SetName();
     }
 }
